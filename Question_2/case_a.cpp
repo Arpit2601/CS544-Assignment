6532cc2fe6581baa345376f1f2a308dc220e70ec
Annanya <annanyapr@gmail.com>
2021-03-11 04:39:02 +0530

    A part done

--- Question_2/case_a.cpp
+++ Question_2/case_a.cpp
@@ -28,8 +28,14 @@ int main(){
     std::random_device rd; 
     std::mt19937 rnd_gen (rd ());
 
-    exponential_distribution<> lambda_generator (lambda_1);
-    exponential_distribution<> mu_generator (mu_1);
+    exponential_distribution<> lambda_generator_1 (lambda_1);
+    exponential_distribution<> lambda_generator_2 (lambda_1);
+    vector<exponential_distribution<>> lambda_generator = {lambda_generator_1, lambda_generator_2};
+
+    exponential_distribution<> mu_generator_1 (mu_1);
+    exponential_distribution<> mu_generator_2 (mu_1);
+    vector<exponential_distribution<>> mu_generator = {mu_generator_1, mu_generator_2};
+
     
     // for(int i = 0; i < 20; i++){
 
@@ -46,82 +52,122 @@ int main(){
     long long completed_migrants = 0;
 
 
-    double next_incoming_timing = current_time + lambda_generator(rnd_gen);
-    int processing_state = 0;
-    double processing_complete_timing = current_time;
+    vector<double> next_incoming_timing = {current_time + lambda_generator[0](rnd_gen), current_time + lambda_generator[1](rnd_gen)};
+    vector<int> processing_state = {0, 0};
+    vector<double> processing_complete_timing = {current_time, current_time};
 
 
-
-    double avg_number_of_migrants_getting_checked = 0;
-    double avg_response_time = 0;
-    double avg_time_for_migrant_in_queue = 0;
-    double avg_number_of_migrant_in_queue = 0;
+    vector<double> avg_number_of_migrants_getting_checked = {0, 0};
+    vector<double> avg_response_time = {0, 0};
+    vector<double> avg_time_for_migrant_in_queue = {0, 0};
+    vector<double> avg_number_of_migrant_in_queue = {0, 0};
     long long total_migrants = 1000000;
+    vector<long long> migrants = {0, 0};
 
-    queue<Timing> waiting_q;
-    Timing currently_processing;
+
+    vector<queue<Timing>> waiting_q = {queue<Timing> (), queue<Timing> ()};
+    vector<Timing> currently_processing = {{0, 0, 0}, {0, 0, 0}};
 
 
 
 
     while(completed_migrants < total_migrants){
 
-        if(processing_state == 0){
-            // The border agent is sitting empty, directly move to the time when the next incoming migrant will come
-            current_time = next_incoming_timing;
-            currently_processing = {current_time, current_time, 0};
 
-            next_incoming_timing = current_time + lambda_generator(rnd_gen);
-            processing_complete_timing = current_time + mu_generator(rnd_gen);
-            processing_state = 1;        
+        vector<double> times;
+
+        for(int i = 0; i < 2; i++){
+            times.push_back(next_incoming_timing[i]);
+        }
+
+
+        for(int i = 0; i < 2; i++){
+            if (processing_state[i] == 1){
+                times.push_back(processing_complete_timing[i]);
+            }
+            else{
+                times.push_back(DBL_MAX);
+            }
+        }
+        
+        int min_index = 0;
+        for(int i = 1; i < 4; i++){
+            if(times[i] < times[min_index]){
+                min_index = i;
+            }
+        }
+
+        if(min_index < 2){
+            // We have an incoming migrant
+            min_index = min_index % 2;
+            current_time = next_incoming_timing[min_index];
+
+            if(processing_state[min_index] == 0){  // The server is empty, directly give the migrant to server
+                currently_processing[min_index] = {current_time, current_time, 0};
+                processing_state[min_index] = 1;
+                processing_complete_timing[min_index] = current_time + mu_generator[min_index](rnd_gen);
+            }
+            else{  // The server is busy,  deposit the migrant to the queue
+                waiting_q[min_index].push({current_time, 0, 0});
+            }
+
+            next_incoming_timing[min_index] = current_time + lambda_generator[min_index](rnd_gen);
 
         }
         else{
-            // The border agent is not empty
-            if (next_incoming_timing <= processing_complete_timing){
-            // Next migrant is coming
-                current_time = next_incoming_timing;
-                next_incoming_timing = current_time + lambda_generator(rnd_gen);
-                waiting_q.push({current_time, 0, 0});
+            // We have an outgoing migrant
+            min_index = min_index % 2;
+            current_time = processing_complete_timing[min_index];
+            currently_processing[min_index].z = current_time;
+
+            avg_number_of_migrants_getting_checked[min_index] += (currently_processing[min_index].z - currently_processing[min_index].y);
+            avg_response_time[min_index] += (currently_processing[min_index].z - currently_processing[min_index].x);
+            avg_time_for_migrant_in_queue[min_index] += (currently_processing[min_index].y - currently_processing[min_index].x); 
+            avg_number_of_migrant_in_queue[min_index] += (currently_processing[min_index].y - currently_processing[min_index].x);
+
+            migrants[min_index]++;
+            completed_migrants++;
+            if(completed_migrants == total_migrants){
+                break;
+            }
+
+            if(waiting_q[min_index].empty()){  // Q is empty change the processing state to 1
+                processing_state[min_index] = 0;
             }
             else{
-            // Migrant is getting processed and leaving
-                current_time = processing_complete_timing;
-                currently_processing.z = current_time;
-
-                avg_number_of_migrants_getting_checked += (currently_processing.z - currently_processing.y);
-                avg_response_time += (currently_processing.z - currently_processing.x);
-                avg_time_for_migrant_in_queue += (currently_processing.y - currently_processing.x); 
-                avg_number_of_migrant_in_queue += (currently_processing.y - currently_processing.x);
-                completed_migrants += 1;
-
-                if (completed_migrants == total_migrants) break;
-
-                // Bring the next one from the queue;
-                if(waiting_q.empty()){  // Witing Queue is empty
-                    processing_state = 0;
-                }
-                else{  
-                    currently_processing =  waiting_q.front();
-                    waiting_q.pop();
-                    currently_processing.y = current_time;
-                    processing_complete_timing = current_time + mu_generator(rnd_gen);
-                }
-
+                currently_processing[min_index] = waiting_q[min_index].front();
+                waiting_q[min_index].pop();
+                currently_processing[min_index].y = current_time;
+                processing_complete_timing[min_index] = current_time + mu_generator[min_index](rnd_gen);
             }
+
+
         }
+
     }
 
 
-    avg_number_of_migrants_getting_checked /= current_time;
-    avg_response_time /= total_migrants;
-    avg_time_for_migrant_in_queue /= total_migrants;
-    avg_number_of_migrant_in_queue /= current_time;
+    for(int i = 0; i < 2; i++){
 
-    cout << avg_number_of_migrants_getting_checked << endl;
-    cout << avg_response_time << endl;
-    cout << avg_time_for_migrant_in_queue << endl;
-    cout << avg_number_of_migrant_in_queue << endl;
+        avg_number_of_migrants_getting_checked[i] /= current_time;
+        avg_response_time[i] /= migrants[i];
+        avg_time_for_migrant_in_queue[i] /= migrants[i];
+        avg_number_of_migrant_in_queue[i] /= current_time;
+
+    }
+
+
+    for(int i = 0; i < 2; i++){
+
+        cout << avg_number_of_migrants_getting_checked[i] << endl;
+        cout << avg_response_time[i] << endl;
+        cout << avg_time_for_migrant_in_queue[i] << endl;
+        cout << avg_number_of_migrant_in_queue[i] << endl;
+
+
+
+    }
+
 
 
 
